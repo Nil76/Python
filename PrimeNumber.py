eebcffaeac5bd54ccc74787347d914bce17a772c
Nilesh Mourya <mouryanilesh43@outlook.com>
2024-08-08 19:45:51 +0530

    Add files via upload

---
+++ PrimeNumber.py
@@ -0,0 +1,11 @@
+num = int(input("Enter Value: "))
+#https://linktr.ee/nilesh7635
+if num > 1:
+    for i in range(2, (num//2)+1):
+        if (num % i) == 0:
+            print(num,  "This is not a prime number")
+            break
+    else:
+        print(num,  "is a prime number")
+else:
+    print(num,  "is not a prime number")
